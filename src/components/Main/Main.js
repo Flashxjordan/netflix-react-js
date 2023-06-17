1df833aaf0adc19f45ab43fd974301c3b12ea327
David Jordan <wordnerd92@gmail.com>
2023-06-17 17:22:20 -0500

    Main component is generating 2 images instead of one so I'm taking a break

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,35 +1,29 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import Requests from "/Users/user/Desktop/netflix-react-js/src/Requests";
+import requests from "../../requests";
 
 const Main = () => {
-        const [movies, setMovies] = useState([])
-
-        const movie = movies[Math.floor(Math.random() * movies.length)];
-
-
-        useEffect(() => {
-            axios.get(Requests.requestPopular).then((response) => {
-              setMovies(response.data.results);
-            });
-          }, []);
-          console.log(movies)
-
-    return (
-        <div className='w-full h-[600px] text-white'> 
-            <div className='w-full h-full'>
-                <div className='absolute w-full h-[600px] bg-gradient-to-r from-black '></div>
-                <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-                <div className='absolute w-full top-[20%] p-4 md:p-8'>
-                <div>
-                    <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'> Play </button>
-                    <button className='border text-white border-gray-300 py-2 px-5 ml-4'> Watch Later </button> 
-                </div>
-                </div>
-                
-            </div>
+    const [movies, setMovies] = useState([]);
+  
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+  
+    useEffect(() => {
+      axios.get(requests.requestTrending).then((response) => {
+        setMovies(response.data.results);
+      });
+    }, []);
+    //   console.log(movie);
+  
+   return (
+      <div className='w-full h-[550px] text-white'>
+        <div className='w-full h-full'>
+            <img 
+            src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+            alt={movie?.title}
+            />
         </div>
-    )
-}
-
-export default Main
+      </div>
+    );
+  };
+  
+  export default Main;
